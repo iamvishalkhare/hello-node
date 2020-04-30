b8645aaeea15dd83a231504cc08d4ddf314a865e
Vishal <vishalkhare39@gmail.com>
2020-05-01 02:24:37 +0530

    Committing procfile

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 // Express Initialize
 const app = express();
-app.get('/hello', function (req, res) {
+app.get('/', function (req, res) {
 res.send('Hello World! from Vishal');
 });
 
-app.listen(8000,()=> {
+app.listen(process.env.PORT || 8000,()=> {
 console.log('listen port 8000');
 });

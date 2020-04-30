b26229b253003cc79d2f717a1f4baa4531ee25eb
Vishal <vishalkhare39@gmail.com>
2020-05-01 02:56:41 +0530

    Fixing bug in Procfile

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 // Express Initialize
 const app = express();
 app.get('/', function (req, res) {
-res.send('Hello World! from Vishal');
+    res.send('Hello World! from Vishal');
 });
 
-app.listen(process.env.PORT || 8000,()=> {
-console.log('listen port 8000');
+app.listen(process.env.PORT || 8000, () => {
+    console.log('listen port 8000');
 });
